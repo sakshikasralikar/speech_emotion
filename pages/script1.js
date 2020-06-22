2301ceac8ea855a95cbd096e3e966d5e7318716d
sakshikasralikar <65922227+sakshikasralikar@users.noreply.github.com>
2020-06-22 11:57:33 +0530

    Add files via upload

---
+++ pages/script1.js
@@ -0,0 +1,7 @@
+    console.log("abc");
+    
+    var url = require('url');
+    var adr = "https://colab.research.google.com/drive/1g_rojY93NSHsfpN3vwXvJbrpOPMi5pPM"
+    var q = url.parse(adr, true);
+    var a =q.y_predict
+    document.getElementById("content").textContent = a; 
