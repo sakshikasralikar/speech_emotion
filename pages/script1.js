eaab705280074ca910f5e96ecdd591ecd37603bc
sakshikasralikar <65922227+sakshikasralikar@users.noreply.github.com>
2020-06-27 01:36:03 +0530

    Update script1.js

--- pages/script1.js
+++ pages/script1.js
@@ -4,7 +4,4 @@
     var adr = "https://colab.research.google.com/drive/1g_rojY93NSHsfpN3vwXvJbrpOPMi5pPM"
     var q = url.parse(adr, true);
     var a =q.y_predict
-    document.getElementById("content").textContent = a; 
-      function myFunction() {
-  document.getElementById("here").innerHTML = "happy";
-}
+   
